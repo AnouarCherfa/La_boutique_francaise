f39de2af0be2e8e38a8bab9d188c49c62e04afc2
anouar <anouarcherfa@outlook.fr>
2022-11-06 15:58:20 +0100

    Ajout du formulaire connexion

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -44,8 +44,18 @@ class RegisterType extends AbstractType
                 'invalid_message'   => 'Le mot de passe et la coonfirmation doivent etre identique.',
                 'label'             => 'Votre mot de passe',
                 'required'          => true,
-                'first_options'       => ['label'    => 'Mot de passe'],
-                'second_options'     => ['label'    => 'Confirmez votre mot de passe']
+                'first_options'       => [
+                    'label'    => 'Mot de passe',
+                    'attr'     => [
+                        'placeholder' => 'Merci de saisir votre mot de passe.'
+                    ]
+                ],
+                'second_options'     => [
+                    'label'    => 'Mot de passe',
+                    'attr'     => [
+                        'placeholder' => 'Merci de confirmer votre mot de passe.'
+                    ]
+                ]
 
             ])
             ->add('submit', SubmitType::class, [
