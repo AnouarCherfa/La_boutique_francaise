345600be5cd49da8094601c2fce21d55c95ca97d
anouar <anouarcherfa@outlook.fr>
2022-11-08 19:21:53 +0100

    Mise à jour footer des templates

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends AbstractController
 {
@@ -18,13 +21,26 @@ class ProductController extends AbstractController
     }
 
     #[Route('/nos-produits', name: 'products')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $search = new Search();
+        $form = $this->createForm( SearchType::class, $search);
 
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
+        ## Ecouter si le formulaire a été soumit
+        $form->handleRequest($request);
 
-        return $this->render('product/index.html.twig', [
-            'products'  => $products
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+            
+        } else {
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
+
+        return $this->renderForm('product/index.html.twig', [
+            'products'  => $products,
+            'form'      => $form
+
         ]);
     }
 
